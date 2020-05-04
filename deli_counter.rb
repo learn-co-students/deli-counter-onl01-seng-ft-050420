2649c3862521249b46e028fdd3ad518686c3d5ff
Luke Chen <luke.chen.2010@gmail.com>
2020-05-04 21:19:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(customers)
+  if(customers.size == 0)
+    puts "The line is currently empty."
+  else
+    line_string = "The line is currently:"
+    
+    customers.each do |customer|
+      line_string = line_string + " #{customers.index(customer) + 1}. #{customer}"
+    end
+    
+    puts line_string
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  if(line.size == 0)
+    puts "There is nobody waiting to be served!"
+  else
+     puts "Currently serving #{line.shift}."
+  end
+end
